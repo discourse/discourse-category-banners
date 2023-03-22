73a7d46cb4bdf4a491c51149b227333fc3751e11
awesomerobot <kris.aubuchon@discourse.org>
2023-03-22 14:47:32 -0400

    fix lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   plugins: ["ember-template-lint-plugin-discourse"],
   extends: "discourse:recommended",
+  rules: {
+    "no-capital-arguments": false, // also false in Discourse, we use "args" a lot
+  },
 };
