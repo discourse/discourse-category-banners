f36252d738eadb4a2b2c2c878b578a5371b04451
awesomerobot <kris.aubuchon@discourse.org>
2023-03-27 13:37:24 -0400

    remove lint rule

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,7 +1,4 @@
 module.exports = {
   plugins: ["ember-template-lint-plugin-discourse"],
   extends: "discourse:recommended",
-  rules: {
-    "no-capital-arguments": false, // also false in Discourse, we use "args" a lot
-  },
 };
