676c5995fd7b70dcc7867adcd0b1f1adc64470a1
awesomerobot <kris.aubuchon@discourse.org>
2023-04-24 15:29:18 -0400

    prettier

--- javascripts/discourse/services/category-banner-presence.js
+++ javascripts/discourse/services/category-banner-presence.js
@@ -1,4 +1,4 @@
-import Service from '@ember/service';
+import Service from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 
 export default class CategoryBannerPresence extends Service {
