d61629c0fc51e556df6baa142748e88d33b8a5d3
Jarek Radosz <jradosz@gmail.com>
2024-03-27 17:16:16 +0100

    DEV: Update linting (#47)

--- javascripts/discourse/components/category-banner.js
+++ javascripts/discourse/components/category-banner.js
@@ -1,10 +1,10 @@
-import Category from "discourse/models/category";
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
-import { htmlSafe } from "@ember/template";
 import { getOwner } from "@ember/application";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
+import { htmlSafe } from "@ember/template";
+import Category from "discourse/models/category";
 
 export default class DiscourseCategoryBanners extends Component {
   @service router;
